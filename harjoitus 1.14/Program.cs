639b73123ee3c24f97247fdf8c74ff949e16a5fb
Randray <jhfanta@gmail.com>
2016-01-19 19:48:54 +0200

    Stuff to do and more programs

--- harjoitus 1.14/Program.cs
+++ harjoitus 1.14/Program.cs
@@ -10,6 +10,89 @@ namespace harjoitus_1._14
     {
         static void Main(string[] args)
         {
+            int zero = 0;
+            int one = 0;
+            int two = 0;
+            int three = 0;
+            int four = 0;
+            int five = 0;
+            int numbers = 0;
+
+            do
+            {
+                Console.WriteLine("Give me your score(end program with the number 6");
+                numbers = int.Parse(Console.ReadLine());
+
+                if (numbers == 0)
+                    zero++;
+                else if (numbers == 1)
+                    one++;
+                else if (numbers == 2)
+                    two++;
+                else if (numbers == 3)
+                    three++;
+                else if (numbers == 4)
+                    four++;
+                else if (numbers == 5)
+                    five++;
+                else if (numbers == 6)
+                    Console.WriteLine("Bye Bye");
+                else
+                    Console.WriteLine("I do not know this score!");
+
+
+            } while (numbers != 6);
+
+            Console.WriteLine("");
+            Console.Write("0: ");
+            for(int i=0; i<zero ;i++)
+            {
+                Console.Write("*");
+
+            }
+
+            Console.WriteLine("");
+            Console.Write("1: ");
+            for (int i = 0; i < one; i++)
+            {
+                Console.Write("*");
+
+            }
+
+
+            Console.WriteLine("");
+            Console.Write("2: ");
+            for (int i = 0; i < two; i++)
+            {
+                Console.Write("*");
+
+            }
+
+
+            Console.WriteLine("");
+            Console.Write("3: ");
+            for (int i = 0; i < three; i++)
+            {
+                Console.Write("*");
+
+            }
+
+
+            Console.WriteLine("");
+            Console.Write("4: ");
+            for (int i = 0; i < four; i++)
+            {
+                Console.Write("*");
+
+            }
+
+            Console.WriteLine("");
+            Console.Write("5: ");
+            for (int i = 0; i < five; i++)
+            {
+                Console.Write("*");
+
+            }
         }
     }
 }
